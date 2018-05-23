5f5df08d0ec58b020d1f3200c6bbb643002ecfd9
Punnadit Trussat <punnadittr@gmail.com>
2018-05-23 12:42:08 +0700

    added possible moves for pawns

--- chess_board.rb
+++ chess_board.rb
@@ -1,38 +1,63 @@
 require "colorize"
 # create a chessboard and function to assign item to the board
 class Board
+  attr_accessor :board
+
+  def board
+    @@board
+  end
+
+  def initialize
+    @@board = [
+      [" "," "," "," "," "," "," "," "],
+      [" "," "," "," "," "," "," "," "],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [" "," "," "," "," "," "," "," "],
+      [" "," "," "," "," "," "," "," "]
+    ]
+  end
 
   def put_piece
 
   end
 
   def create_board
-    board = [
-      ["\u2656","\u2658","\u2657","\u2655","\u265A","\u2657","\u2658","\u2656"],
-      ["\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659"],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      [' ',' ',' ',' ',' ',' ',' ',' '],
-      ["\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659"],
-      ["\u2656","\u2658","\u2657","\u2655","\u265A","\u2657","\u2658","\u2656"]
-    ]
+    
   end
 
   def print_board
-    board.each_with_index do |row, i|
+    @@board.reverse.each_with_index do |row, i|
       row.each_with_index do |piece, index|
         if i.even?
           if index.even?
-            colorme(piece, :black, :cyan)
+            if piece == " "
+              colorme(piece, :black, :cyan)
+            else
+              colorme(piece.display, :black, :cyan)
+            end
           else
-            colorme(piece, :black, :white)
+            if piece == " "
+              colorme(piece, :black, :white)
+            else
+              colorme(piece.display, :black, :white)
+            end
           end
         else
           if index.even?
-            colorme(piece, :black, :white)
+            if piece == " "
+              colorme(piece, :black, :white)
+            else
+              colorme(piece.display, :black, :white)
+            end
           else
-            colorme(piece, :black, :cyan)
+            if piece == " "
+              colorme(piece, :black, :cyan)
+            else
+              colorme(piece.display, :black, :cyan)
+            end
           end
         end
       end
@@ -44,11 +69,32 @@ class Board
     print 
     print (' ' + object + ' ').colorize(:color => main_color, :background => bg_color )
   end
-
 end
 
+require_relative "chess_piece"
+
+myboard = Board.new
+pawn1 = Pawn.new 2,4
+myboard.board[4][2] = pawn1
+pawn2 = Pawn.new 3,5
+pawn3 = Pawn.new 1,5
+myboard.board[5][3] = pawn2
+myboard.board[5][1] = pawn3
+myboard.print_board
+pawn1.legal_moves
+pawn1.capture_moves
+myboard.print_board
 
 
-
-
-
+=begin
+    @@board = [
+      ["\u2656","\u2658","\u2657","\u2655","\u265A","\u2657","\u2658","\u2656"],
+      ["\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659"],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ',' ',' ',' '],
+      ["\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659","\u2659"],
+      ["\u2656","\u2658","\u2657","\u2655","\u265A","\u2657","\u2658","\u2656"]
+    ]
+=end
--- chess_piece.rb
+++ chess_piece.rb
@@ -1,44 +1,56 @@
-# defines rule for every piece
-class Piece
+class Pawn < Board
+  attr_reader :possible_moves
 
-end
-
-
-class Pawn
-  def initialize(piece_color, position)
-    @piece_color = piece_color
-    @position = position
-  end
-
-  def legal_moves
-
-  end
-
-  def capture
-
+  def initialize(x,y)
+    @x = x
+    @y = y
+    @position = [x,y]
+    @first_move = true
+    @possible_moves = []
+    @legal_moves = []
+    @capture_moves = []
   end
 
   def display
-
+    "\u2659"
   end
-end
 
-class Rook
+  def legal_moves
+    if @@board[@y+1][@x] == " "
+      @legal_moves << [@x, @y+1]
+    end
+    if @first_move && @@board[@y+2][@x] == " " && @@board[@y+1][@x] == " "
+      @legal_moves << [@x, @y+2] 
+    end
+    @possible_moves << @legal_moves
+  end
 
-end
+  def capture_moves
+    x = @x - 1
+    y = @y + 1
+    2.times do
+      if @@board[y][x] != nil && @@board[y][x] != " " 
+        @capture_moves << [x, y]
+      end
+      x += 2
+    end
+    @possible_moves << @capture_moves
+    @capture_moves
+  end
 
-class Knight
-
-end
-
-class Bishop
-
-end
-
-class Queen
-
-end
-
-class King
-
+  def move(x_co, y_co)
+    current_x = @x
+    current_y = @y
+    if @possible_moves.include? [x_co, y_co]
+      @@board[current_y][current_x] = ' '
+      @@board[y_co][x_co] = self
+      @x = x_co
+      @y = y_co
+      @position = [@x, @y]
+      @first_move = false
+      @possible_moves = []
+    else
+      "INVALID MOVE"
+    end
+  end
 end
